d75e4e60a754d0b76d7d2a406db6b21b4b9c0033
Mrcsphllps <mrcsphllps@gmail.com>
2026-04-23 16:16:59 +0300

    initial commit

---
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -0,0 +1,23 @@
+package com.example.polls;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PollsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+
+}
